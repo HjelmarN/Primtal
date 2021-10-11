da54675bb4d5d69037ab30c1b15e09403e77dd12
Elias Hjelm <hjelmarn@protonmail.com>
2021-10-11 11:29:54 +0200

    Added comments to explain the code.

--- Primtal/Prime.cs
+++ Primtal/Prime.cs
@@ -6,9 +6,9 @@ namespace Primtal
 {
     internal class Prime
     {
-        private bool keepGoing = true;
+        private bool keepGoing = true; //Bool which I use as the condition in my while loop in the Menu method.
 
-        private List<int> primeList = new List<int>();
+        private List<int> primeList = new List<int>(); //List where the prime numbers gets added
 
         public void Menu()
         {
@@ -18,13 +18,13 @@ namespace Primtal
 
             try
             {
-                var input = Convert.ToInt32(userInput);
+                var input = Convert.ToInt32(userInput); //Error handling in case the user inputs something wrong.
                 while (keepGoing)
                 {
                     switch (input)
                     {
                         case 0:
-                            keepGoing = false;
+                            keepGoing = false; //Stops the while loop, so the program stops. 
                             break;
 
                         case 1:
@@ -33,7 +33,7 @@ namespace Primtal
                             break;
 
                         case 2:
-                            NextPrime(primeList.LastOrDefault());
+                            NextPrime(primeList.LastOrDefault()); //Uses the last prime number in the primeList as the parameter. Works because I sorted the list already.
                             Menu();
                             break;
 
@@ -43,6 +43,7 @@ namespace Primtal
                             break;
 
                         default:
+                            Console.Clear();
                             Console.WriteLine("There is no corresponding menu choice, try again!");
                             Menu();
                             break;
@@ -74,6 +75,7 @@ namespace Primtal
 
             return true; //If it is not evenly divided it is prime.
         }
+
         //Method that calculates the next prime
         private void NextPrime(int number)
         {
@@ -82,34 +84,34 @@ namespace Primtal
 
             while (!isPrime)
             {
-                var counter = ++number;
+                var counter = ++number; //Starts at number + 1, and increments by 1 each turn.
 
-                if (IsPrime(counter))
+                if (IsPrime(counter)) //Uses the IsPrime method with the counter as the parameter.
                 {
-                    primeList.Add(counter);
-                    isPrime = true;
-                    Console.WriteLine("Prime number has been added.");
+                    primeList.Add(counter); //Adds the number counter's value to the list of prime numbers.
+                    isPrime = true; //Updates the isPrime variable to true to cancel the while loop.
+                    Console.WriteLine("Prime number has been added."); //Inform the user that a prime number has been added to the list.
                 }
             }
         }
 
-        //Method informing the user if the input number is prime or not
+        //Informing the user if the input number is prime or not
         private void PrintIfPrime()
         {
             Console.Clear();
             Console.WriteLine("Enter numbers to check if prime");
             var input = Convert.ToInt32(Console.ReadLine());
 
-            if (IsPrime(input))
+            if (IsPrime(input)) //Using the IsPrime method to calculate if prime or not.
             {
                 Console.WriteLine($"{input} is prime");
 
                 if (!primeList.Contains(input))
                 {
-                    primeList.Add(input);
+                    primeList.Add(input); //If the number is prime it gets added to the primeList.
                 }
 
-                primeList.Sort();
+                primeList.Sort(); //Sorts the primeList
             }
             else
             {
@@ -117,6 +119,7 @@ namespace Primtal
             }
         }
 
+        //Prints the different menu choices to inform the user.
         private void PrintMenu()
         {
             Console.WriteLine("Enter 1 to test if numbers are prime or not");
@@ -125,10 +128,11 @@ namespace Primtal
             Console.WriteLine("Enter 0 to exit the program");
         }
 
+        //Prints the prime numbers stored in the primeList.
         private void PrintPrimes()
         {
             Console.Clear();
-            foreach (var item in primeList)
+            foreach (var item in primeList) //Iterates through all the elements in the primeList.
             {
                 Console.WriteLine(item);
             }
